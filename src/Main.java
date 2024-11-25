1c9ed550a7860e99980da4973612213b462f068c
nikolay-pl <s2109014100@sd.uni-vt.bg>
2024-11-25 08:57:41 +0200

    Update Main.java
    I do some changes in the code, and remove part of it, which isn't  in use.

--- src/Main.java
+++ src/Main.java
@@ -6,8 +6,7 @@ import com.sirma.academy.models.Student;
 
 public class Main {
     public static void main(String[] args) {
-        //Student s1 = new Student(22, "George");
-        //Student s2 = new Student(29, "Mihailo");
+
 
         User user = new User("Peter", "Pan");
         User thief = new User("Joe", "Baidan");
@@ -15,11 +14,11 @@ public class Main {
 
         BankServer server = new BankServer();
         Access_account limitedBankAccount = new Access_account(user);
-        //BankAccount acc = server.createAccount(user);
+        BankAccount acc = server.createAccount(user);
         user.addAccount(acc);
 
         try {
-            server.deposit(user, acc, 100);
+            server.deposit(thief, acc, 100);
             server.withdraw(acc, 200);
         } catch (IllegalAccessException e){
             System.out.println(e.getMessage());
